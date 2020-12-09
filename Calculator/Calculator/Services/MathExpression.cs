e176376b79610365ff8024d8a2c15b374c258c63
Blackbird625 <59508883+Blackbird625@users.noreply.github.com>
2020-12-09 20:24:47 +0100

    Add MathExpression parser mock

---
+++ Calculator/Calculator/Services/MathExpression.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Services
+{
+	public class MathExpression
+	{
+		public double Parse(string expression)
+		{
+			return 0.0;
+		}
+
+		public bool TryParse(string expression, out double result)
+		{
+			try
+			{
+				result = Parse(expression);
+				return true;
+			}
+			catch
+			{
+				result = default;
+				return false;
+			}
+
+		}
+	}
+}
